2ea49b051e03a5f260c86d8bf5a6ce4571bed23a
Morgan Nelson <mnelson@steele-forge.com>
2012-06-25 17:02:37 -0500

    finalizing pruning

--- lib/appshot/volume/ebs_volume.rb
+++ lib/appshot/volume/ebs_volume.rb
@@ -5,23 +5,50 @@ require 'awesome_print'
 class EBSVolume < Volume
   include Fog
 
-  def initialize(fog_compute)
-    @fog = fog_compute
+  attr_accessor :volume_id, :region
+  attr_writer   :aws_access_key_id, :aws_secret_access_key
+  attr_reader   :provider
+
+
+  def initialize(opts = {})
+    @provider = 'AWS'
+    @region = opts[:region] unless opts[:region].nil?
+    @aws_access_key_id = opts[:aws_access_key_id] unless opts[:aws_access_key_id].nil?
+    @aws_secret_access_key = opts[:aws_secret_access_key] unless opts[:aws_secret_access_key].nil?
+
+    @fog = Fog::Compute.new(:provider => @provider,
+                            :region => @region,
+                            :aws_access_key_id => @aws_access_key_id,
+                            :aws_secret_access_key => @aws_secret_access_key,
+                           )
   end
 
   def volumes
     @fog.volumes.all
   end
 
-  def servers
-    @fog.servers.all
+  def snap(volume_id, description = "", options = {})
+    snap = @fog.snapshots.create({:volume_id => volume_id, :description => description}.merge(options))
+    while snap.state != "completed" do
+      sleep 0.5
+      snap.reload
+    end
+    snap
   end
 
-  def mocking?
-    Fog.mocking?
+  def snapshots_for(volume_id)
+    # for more info on Amazon EC2 filters (like "volume-id" below) please
+    # refer to http://docs.amazonwebservices.com/AWSEC2/latest/APIReference/ApiReference-query-DescribeVolumes.html
+    @fog.snapshots.all("volume-id" => volume_id)
   end
 
-  def mock_reset
-    @fog.reset
+  def prune_snapshots(volume_id, snapshots_to_keep, not_after_time = Time.now)
+    puts "At entry, not_after_time is: #{not_after_time}"
+    snapshots = snapshots_for(volume_id)
+    (snapshots.count - snapshots_to_keep).times do
+      puts "snapshot create: #{snapshots.first.created_at} < #{not_after_time}"
+      snapshots.first.destroy if snapshots.first.created_at < not_after_time
+      snapshots.reload
+    end
   end
 end
--- spec/lib/appshot/volume/ebs_volume_spec.rb
+++ spec/lib/appshot/volume/ebs_volume_spec.rb
@@ -3,63 +3,122 @@ require "appshot"
 require "appshot/volume"
 require "appshot/volume/ebs_volume"
 
-# the ebs class should be able to:
-# list all volumes
-# snapshot a volume
-# delete a snapshot
 
 describe EBSVolume do
-  def create_server(zone = "us-east-1" + %w(a b c d).sample)
-    delay = Fog::Mock.delay
-    puts "Mock delay = #{delay}"
-    Fog::Mock.delay = 0
-    current_server_count = fog_compute.servers.all.count
-    new_server = fog_compute.servers.create
-    new_server.reload.id
-    Fog::Mock.delay = delay
-  end
-
   before do
     Fog::Compute::AWS::Mock.reset
     Fog.mock!
   end
 
-  let(:fog_compute) do
-    Fog::Compute.new(:provider => 'AWS',
-                     :region => region,
-                     :aws_access_key_id => aws_access_key_id,
-                     :aws_secret_access_key => aws_secret_access_key,
-                    )
-  end
-  let(:ebs) { described_class.new(fog_compute) }
+  let(:ebs) { described_class.new(:provider => 'AWS',
+                                  :region => region,
+                                  :aws_access_key_id => aws_access_key_id,
+                                  :aws_secret_access_key => aws_secret_access_key,
+                                 ) }
+  let(:fog_compute) { ebs.instance_variable_get(:@fog)  }
   let(:region) { "us-east-1" }
   let(:aws_access_key_id) { "access_key_shhh" }
   let(:aws_secret_access_key) { "secret_key_shhh" }
 
-  describe "basics" do
+  describe "basic setup" do
     it "should be a class" do
       described_class.should be_a_kind_of Class
     end
+
+    it "should have a volume_id" do
+      ebs.respond_to?(:volume_id).should be_true
+    end
   end
 
+  describe "given a volume_id" do
+    let(:snapshot_count) { 2 }
+    let(:volume_id)  { volumes.first.id }
 
-  describe "manipulating EBS volumes" do
-    context "Mock.delay == 3" do
-      Fog::Mock.delay = 3
-
-      it "must not return until the volume is available" do
-        create_server
-        ebs.servers.count.should == 1
+    let(:volumes) do
+      with_no_mock_delay do
+        [ create_volume("us-east-1a"),
+          create_volume("us-east-1b"), ]
       end
     end
 
-    context "Mock.delay == 0" do
-      Fog::Mock.delay = 0
+    let(:snaps) do
+      with_no_mock_delay do
+        snapshot_count.times do |i|
+          ebs.snap(volume_id, "This is test snapshot ##{i}")
+        end
+        ebs.snapshots_for(volume_id)
+      end
+    end
 
-      it "should create three servers" do
-        3.times { create_server }
-        ebs.servers.count.should == 3
+    it "should create a snap id with a valid format" do
+      snaps.first.id.should =~ /^snap-*/
+    end
+
+    it "must finish a snapshot of the volume" do
+      snaps.first.state.should == "completed"
+    end
+
+    it "should know how many snapshots there are for a given volume" do
+      snaps
+      ebs.snapshots_for(volumes.first.id).count.should == 2
+      ebs.snapshots_for(volumes.last.id).count.should == 0
+    end
+
+    describe "pruning snapshots" do
+      context "with a minimum age" do
+        let(:snapshot_count) { 5 }
+
+        it "should not prune snapshots younger than the minimum age" do
+          with_no_mock_delay do
+            ebs.snap(volume_id, "This is test snapshot #0", :created_at => Time.now - (4 * 86400) )
+            ebs.snap(volume_id, "This is test snapshot #1", :created_at => Time.now - (3 * 86400) )
+            ebs.snap(volume_id, "This is test snapshot #2", :created_at => Time.now - (2 * 86400) )
+            ebs.snap(volume_id, "This is test snapshot #3", :created_at => Time.now - (1 * 86400) )
+            ebs.snap(volume_id, "This is test snapshot #4", :created_at => Time.now - (3600) )
+          end
+          ebs.snapshots_for(volumes.first.id).count.should == 5
+          ebs.prune_snapshots(volumes.first.id, 1, Time.now - (2 * 86399))
+          ebs.snapshots_for(volumes.first.id).count.should == 3
+        end
+      end
+      context "without a minimum age" do
+        let(:snapshot_count) { 3 }
+
+        it "should prune snapshots for a given volume to a snapshot count" do
+          snaps
+          ebs.snapshots_for(volumes.first.id).count.should == 3
+          ebs.prune_snapshots(volumes.first.id, 1)
+          ebs.snapshots_for(volumes.first.id).count.should == 1
+        end
       end
     end
   end
 end
+
+private
+
+def create_server(zone = "us-east-1" + %w(a b c d).sample)
+  with_no_mock_delay do
+    new_server = fog_compute.servers.create
+    new_server.reload.id
+  end
+end
+
+def create_volume(zone = "us-east-1" + %w(a b c d).sample, size = 20)
+  with_no_mock_delay do
+    new_volume = fog_compute.volumes.create(:availability_zone => zone, :size => size)
+    new_volume.reload
+  end
+end
+
+# The fog library provides awesome mocks, including simulating round trip times to the
+# remote service.  Easily turn it off when not needed.
+def with_no_mock_delay
+  delay = Fog::Mock.delay
+  Fog::Mock.delay = 0
+  return_object = yield
+  Fog::Mock.delay = delay
+  return_object
+end
+
+
